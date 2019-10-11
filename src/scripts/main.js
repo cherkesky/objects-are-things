7daee1ce21a165d0ce551b225bad911a4656a95c
Cherkesky <guy@cherkesky.com>
2019-10-11 14:25:19 -0500

    created the myPet object

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
-console.log ("Kaboom!");
+// Practice: Represent your Pet
+// Create an object that represents your pet.
+
+// Name property with a string value.
+// Species property with a string value.
+// Nicknames property with an array value. Array contains strings.
+// Age property with an integer value
+
+
+const myPet = {
+  name: "Laika",
+  species: "Dog",
+  nicknames: ["Lulu","Lucifer","Creeper"],
+  age: 2
+}
